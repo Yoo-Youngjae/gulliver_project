0360a7d16114ef85e275a80b98c03d332fd5b10b
sujin9905 <sn5735@snu.ac.kr>
2021-07-29 18:15:12 +0900

    modified offset values

--- kinova/move_gen3_lite.py
+++ kinova/move_gen3_lite.py
@@ -16,7 +16,7 @@ from sensor_msgs.msg import PointCloud2
 from cv_bridge import CvBridge, CvBridgeError
 from control_msgs.msg import GripperCommand
 
-sys.path.append('/home/cylee/Pycharmprojects/gulliver_project/kinova/yolo.py')
+sys.path.append('/home/tidy/Pycharmprojects/gulliver_project/kinova/yolo.py')
 from yolo import net, meta, detect, draw_bounding_box
 
 class MoveWithYolo(object):
@@ -68,33 +68,7 @@ class MoveWithYolo(object):
         else:
             self.is_init_success = True
 
-    def home_pose_1(self):
-        # better detection
-        joint_positions = self.arm_group.get_current_joint_values()
-        joint_positions[0] = -1.6807167028912628
-        joint_positions[1] = -0.931138588787598
-        joint_positions[2] = -0.2665328903206561
-        joint_positions[3] = -0.4130190008160284
-        joint_positions[4] = -2.530036058772793
-        joint_positions[5] = -2.2814262425883705
-        self.arm_group.set_joint_value_target(joint_positions)
-        self.arm_group.go(wait=True)
-        return self.arm_group.get_current_pose().pose
-
-    def home_pose_2(self):
-        # better convenience
-        joint_positions = self.arm_group.get_current_joint_values()
-        joint_positions[0] = 0.2792223202826658
-        joint_positions[1] = -0.4822542649392556
-        joint_positions[2] = -0.1681498606987306
-        joint_positions[3] = -1.7796536700199246
-        joint_positions[4] = -2.1946516658419917
-        joint_positions[5] = -1.4156512143312403
-        self.arm_group.set_joint_value_target(joint_positions)
-        self.arm_group.go(wait=True)
-        return self.arm_group.get_current_pose().pose
-
-    def home_pose_3(self):
+    def home_pose(self):
         joint_positions = self.arm_group.get_current_joint_values()
         joint_positions[0] = -0.07124435284958164
         joint_positions[1] = 0.28432546956347565
@@ -132,53 +106,18 @@ class MoveWithYolo(object):
         y = center[1]
         return (self.pc[y][x][0], self.pc[y][x][1], self.pc[y][x][2])
 
-    def target_pose_1(self, coordinate):
-        x = coordinate[0]
-        y = coordinate[1]
-        z = coordinate[2]
 
-        cur_pose = self.arm_group.get_current_pose().pose
-        cur_pose.position.x += math.sin(34.42) * (-y) + 0.33 + 0.05
-        cur_pose.position.y += -x + 0.27
-        cur_pose.position.z += -z + 0.2
-
-        return cur_pose
-
-    def target_pose_2(self, coordinate):
-        x = coordinate[0]
-        y = coordinate[1]
-        z = coordinate[2]
-
-        cur_pose = self.arm_group.get_current_pose().pose
-        cur_pose.position.x += math.sin(56) * (-y) + 0.08 + 0.04
-        cur_pose.position.y += -x  # + 0.14
-        cur_pose.position.z += -z + 0.2
-
-        return cur_pose
-
-    def target_pose_3(self, coordinate):
+    def target_pose(self, coordinate):
         x = coordinate[0]
         y = coordinate[1]
         z = coordinate[2]
 
         cur_pose = self.arm_group.get_current_pose().pose
         cur_pose.position.x += -y + 0.36
-        cur_pose.position.y += -x - 0.07
-        cur_pose.position.z += -z + 0.2
+        cur_pose.position.y += -x
+        cur_pose.position.z += -z + 0.3
 
         return cur_pose
-
-    def waypoint(self):
-        joint_positions = self.arm_group.get_current_joint_values()
-        joint_positions[0] = -0.45383247715371144
-        joint_positions[1] = -1.0841302691938166
-        joint_positions[2] = 0.3988270153669965
-        joint_positions[3] = 2.1024287941375284
-        joint_positions[4] = 1.2569933923897698
-        joint_positions[5] = 1.057800128128421
-        self.arm_group.set_joint_value_target(joint_positions)
-        self.arm_group.go(wait=True)
-        return self.arm_group.get_current_pose().pose.orientation
 
     def pick(self):
         gripper_joint = self.robot.get_joint('right_finger_bottom_joint')
@@ -204,7 +143,7 @@ if __name__ == '__main__':
     moveclass.center_rgb = (None, None)
 
     print('1. [Start] go to home pose')
-    moveclass.home_pose_3()
+    moveclass.home_pose()
     print('1. [End] go to home pose')
     print('2. [Start] Yolo detection')
     moveclass.disable_img_sub = False
@@ -214,21 +153,16 @@ if __name__ == '__main__':
     print('2. [End] Yolo detection')
     center_pointcloud = moveclass.get_object_pc()
     cur_object_name = moveclass.name
-    print('3.', cur_object_name, 'Target Pose :', moveclass.target_pose_3(center_pointcloud))
-    # # print('4. [Start] go to waypoint')
-    # orientation = moveclass.waypoint()
-    # print('4. [End] go to waypoint')
-    print('5. [Start] go to target pose')
-    # moveclass.target_pose(center_pointcloud).orientation = orientation
-    moveclass.arm_group.set_pose_target(moveclass.target_pose_3(center_pointcloud))
+    print('{0} target : {1}'.format(cur_object_name, moveclass.target_pose(center_pointcloud)))
+    print('3. [Start] go to target pose')
+    moveclass.arm_group.set_pose_target(moveclass.target_pose(center_pointcloud))
     moveclass.arm_group.go(wait=True)
-    print('5. [End] go to target pose')
-    print('6. [Start] Pick', cur_object_name)
+    print('3. [End] go to target pose')
+    print('4. [Start] Pick', cur_object_name)
     moveclass.pick()
-    print('6. [End] Pick', cur_object_name)
+    print('4. [End] Pick', cur_object_name)
     moveclass.open_gripper()
-    moveclass.home_pose_3()
-    # print(moveclass.arm_group.get_current_joint_values())
+    moveclass.home_pose()
 
 
 
